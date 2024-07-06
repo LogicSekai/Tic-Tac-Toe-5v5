4bebf2c58a306a08e0a4386bdca4c15e571d2775
LogicSekai <cameliayu112@gmail.com>
2024-07-06 22:13:33 +0700

    commit project

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<!-- <script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script> -->
+
+<template>
+  <!-- <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView /> -->
+  <div class="section">
+    
+  </div>
+</template>
+
+<!-- <style scoped>
+.section{
+  width: 100%;
+  height: 100vh;
+  background: linear-gradient(33deg, rgba(177,192,241,.95) 0%, rgba(80,122,242,.95) 100%);
+}
+</style> -->
